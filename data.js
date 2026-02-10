1f911d423939fc2b991048909528439bfaa93d82
callservicemada-lgtm <callservicemada@gmail.com>
2026-02-10 15:59:31 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+const ddb = [
+  {
+    "anarana": "SAC MODE LUXE 2024",
+    "v_taloha": "95.000 Ar",
+    "vidy": "75.000 Ar",
+    "karazana": "mode",
+    "sary": "https://images.unsplash.com/photo-1584917663913-673b6f682469?w=400",
+    "fb": "https://facebook.com",
+    "tel": "0340000000"
+  },
+  {
+    "anarana": "LAMPE DECO MODERNE",
+    "v_taloha": "110.000 Ar",
+    "vidy": "85.000 Ar",
+    "karazana": "deco",
+    "sary": "https://images.unsplash.com/photo-1507473884658-ec71b3ce99b5?w=400",
+    "fb": "https://facebook.com",
+    "tel": "0340000000"
+  },
+  {
+    "anarana": "KIRARO NIKE AIR MAX",
+    "v_taloha": "160.000 Ar",
+    "vidy": "130.000 Ar",
+    "karazana": "kiraro",
+    "sary": "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=400",
+    "fb": "https://facebook.com",
+    "tel": "0340000000"
+  },
+  {
+    "anarana": "IPHONE 13 PRO MAX",
+    "v_taloha": "3.500.000 Ar",
+    "vidy": "2.900.000 Ar",
+    "karazana": "elektronika",
+    "sary": "https://images.unsplash.com/photo-1632661674596-df8be070a5c5?w=400",
+    "fb": "https://facebook.com",
+    "tel": "0340000000"
+  }
+];
